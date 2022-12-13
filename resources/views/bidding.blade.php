975f9b4984e7ab76c56d5fbdd2c8ad48f497d932
Ars3nicc <franzmanrique21@gmail.com>
2022-12-13 21:54:06 +0800

    updated design

---
+++ resources/views/bidding.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/bidding.css') }}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<header>
+    <div class="title-header">
+        BARKADAPP
+    </div>
+</header>
+
+<body>
+    <div class="main">
+        <form class="bidding-form" action="">
+            <div class="add-bidding-set">
+                <button class="add-bidding">
+                    <i class='bx bx-add-to-queue' ></i>
+                    Add Bidding
+                </button>
+                <i class='bx bx-trash'></i>
+            </div>
+
+            <input class="place-bidding-btn" type="submit" value="Place Bidding">
+
+
+        </form>
+        <img src="/img/party.png" class='home-img'>
+
+
+    </div>
+
+</body>
+
+
+</html>
